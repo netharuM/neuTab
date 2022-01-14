8b56468f40ca48c70e6c3b30740e567abd0cfe3a
netharu methmitha <netharu.m@gmail.com>
2022-01-14 12:49:29 +0530

    aded light theme for whoever wants it 🙃🙃

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,22 @@ const path = require("path");
 module.exports = {
     mode: "production",
     target: "web",
-    devtool: "source-map",
+    devtool: "inline-source-map",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
 };
